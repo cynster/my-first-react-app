6f803525030ea52aaa55d903d819babc6b143661
cynster <marius.korff@live.nl>
2021-11-29 11:14:41 +0100

    Added Netlify redirects

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>The is me, about to destroy your code:</h1>
+        <img src="https://www.rtlnieuws.nl/sites/default/files/content/images/2021/04/30/disaster%20girl.jpg" alt="Girl happy house burn"></img>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World!
         </a>
       </header>
     </div>
